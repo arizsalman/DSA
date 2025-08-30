45e00f8f68e57136c2fb7ac8d39172d447a96b5f
arizsalman <muhammadarizsalman@gmail.com>
2025-08-30 20:06:28 +0500

    Range practice 👍

--- easyques.py
+++ easyques.py
@@ -1,15 +1,22 @@
-# digits = [1,2,3]==>[1,2,4]
-def dig(digits):
-    n = len(digits)
-    for i in range(n-1, -1, -1):
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
-        digits[i] = 0
-    return [1] + digits
+# # digits = [1,2,3]==>[1,2,4]
+# def dig(digits):
+#     n = len(digits)
+#     for i in range(n-1, -1, -1):
+#         if digits[i] < 9:
+#             digits[i] += 1
+#             return digits
+#         digits[i] = 0
+#     return [1] + digits
 
-# [4,3,2,1] 0,1,2,3,=>1,2,3,4 ab  9 = 0 => 1,0 agar ham return me 0 nahe karwte tu requirement puri nahe hote  is leye hum ne 1ko plus kia
+# # [4,3,2,1] 0,1,2,3,=>1,2,3,4 ab  9 = 0 => 1,0 agar ham return me 0 nahe karwte tu requirement puri nahe hote  is leye hum ne 1ko plus kia
 
 
-print(dig([9]))
-# print(dig([1, 2, 3]))
+# print(dig([9]))
+# # print(dig([1, 2, 3]))
+
+number = [-3, -2, -1, 0, 1, 2, 3, 4, ..., 40]
+number = list(range(41))
+# print(number)
+for i in range(10, -11, -2):
+    print(i)
+    
