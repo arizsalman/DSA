d9b8d3e2bf3168e0c7935280b58b7d64bd24b79b
arizsalman <muhammadarizsalman@gmail.com>
2025-09-01 21:51:31 +0500

    Another Example 👍

--- DSA_phase_easy/easy_qu.py
+++ DSA_phase_easy/easy_qu.py
@@ -5,15 +5,28 @@
 #  5, nums = [0,1,4,0,3,_,_,_]
 
 
-def func(nums):
-    slow = 0
-    for fast in range(1, len(nums)):
-        if nums[fast] != nums[slow]:
-            slow += 1
-            nums[fast] = nums[slow]
-    return slow
-# 0,1,2,3,4
+# def func(nums):
+#     slow = 0
+#     for fast in range(1, len(nums)):
+#         if nums[fast] != nums[slow]:
+#             slow += 1
+#             nums[fast] = nums[slow]
+#     return slow
+# # 0,1,2,3,4
 
 
-print(func([3, 2, 2, 3]))
-print(func([0, 1, 2, 2, 3, 0, 4, 2]))
+# print(func([3, 2, 2, 3]))
+# print(func([0, 1, 2, 2, 3, 0, 4, 2]))
+
+
+def remo(nums, val):
+    k = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+
+print(remo(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=2))
+print(remo(nums=[3, 2, 2, 3], val=2))
