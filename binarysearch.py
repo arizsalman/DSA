faeab60e92af377146c480ec59631bd63ba90935
arizsalman <muhammadarizsalman@gmail.com>
2025-09-07 00:16:49 +0500

    Some practice 👍

--- binarysearch.py
+++ binarysearch.py
@@ -1,16 +1,32 @@
-def func(nums,  target):
+# def func(nums,  target):
+#     left, right = 0, len(nums)-1
+#     while left <= right:
+#         # mid = (left+right) % 2
+#         mid = (left+right)//2
+#         if nums[mid] == target:
+#             return mid
+#         elif nums[mid] < target:
+#             left = mid+1
+#         else:
+#             right = mid-1
+#     return -1
+
+
+# # print(func([-1, 0, 3, 5, 9, 12], 9))
+# print(func(nums=[-1, 0, 3, 5, 9, 12], target=9))
+
+
+def func(nums):
+
     left, right = 0, len(nums)-1
-    while left <= right:
-        # mid = (left+right) % 2
+    while left < right:
         mid = (left+right)//2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] < target:
+        if nums[mid] < nums[mid+1]:
             left = mid+1
         else:
             right = mid-1
-    return -1
+    
+    return left
 
 
-# print(func([-1, 0, 3, 5, 9, 12], 9))
-print(func(nums=[-1, 0, 3, 5, 9, 12], target=9))
+print(func([1, 2, 3, 1]))
--- easy_Q.py
+++ easy_Q.py
@@ -3,15 +3,42 @@
 # order = [3, 1, 2, 5, 4], friends = [1, 3, 4]
 
 
-def func(order, friends):
+# def func(order, friends):
 
-    n = []
+#     n = []
 
-    for i in order:
-        if i in friends:
-            n.append(i)
+#     for i in order:
+#         if i in friends:
+#             n.append(i)
 
-    return n
+#     return n
 
 
-print(func([3, 1, 2, 5, 4], [1, 3, 4]))
+# print(func([3, 1, 2
+# , 5, 4], [1, 3, 4]))
+
+
+# def func(nums):
+#     seen = set()
+#     for i in nums:
+#         if i in seen:
+#             return True
+#         seen.add(i)
+#     return False
+
+
+# print(func(nums=[1, 2, 3, 1]))
+
+
+def func(nums):
+    seem = set()
+    for i in nums:
+        if i in seem:
+            return True
+        else:
+            seem.add(i)
+    return False
+
+
+print(func([1, 2, 3, 4, 5, 6, 7, 0]))
+print(func([1,  2, 3, 4, 5, 6, 7, 8, 9, 3, 2, 1]))
