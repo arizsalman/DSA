0fe8d613df10b1fc6ff445496d21eede70090766
arizsalman <muhammadarizsalman@gmail.com>
2025-09-07 23:54:42 +0500

    Some Practice ◀️

--- easy_Q.py
+++ easy_Q.py
@@ -30,15 +30,38 @@
 # print(func(nums=[1, 2, 3, 1]))
 
 
-def func(nums):
-    seem = set()
-    for i in nums:
-        if i in seem:
-            return True
+# def func(nums):
+#     seem = set()
+#     for i in nums:
+#         if i in seem:
+#             return True
+#         else:
+#             seem.add(i)
+#     return False
+
+
+# print(func([1, 2, 3, 4, 5, 6, 7, 0]))
+# print(func([1,  2, 3, 4, 5, 6, 7, 8, 9, 3, 2, 1]))
+
+
+def func(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+    '''Kyoki closing bracket aate hi check karna hota hai uska matching opening bracket stack ke top pe hai ya nahi, isliye mapping me keys hamesha closing brackets hote hain. ✅'''
+    for char in s:
+        if char in mapping:
+            print(f' d stack {stack}')
+            top = stack.pop() if stack else '#'
+            print(f' is stack : {stack}')
+            if mapping[char] != top:
+                return False
         else:
-            seem.add(i)
-    return False
+            stack.append(char)
+
+    return not stack
 
 
-print(func([1, 2, 3, 4, 5, 6, 7, 0]))
-print(func([1,  2, 3, 4, 5, 6, 7, 8, 9, 3, 2, 1]))
+print(func("([])"))
+# print(func("({{}])"))        # True
+# print(func("()[]{}"))    # True
+# print(func("{[]}"))      # True
